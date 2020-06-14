e01fd6b244a38cf4733ab527db6da9b716dae55c
Ajith Joseph <syriaccjoseph@gmail.com>
2020-06-14 20:57:39 +0530

    initial commit
    WallpapersServer with Spring Boot
    README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# Requirements
+Java 8+<br>
+Maven 3.2+
+
+# Installation
+
+#### MacOS/Linux
+```shell script
+./mvnw spring-boot:run
+```
+
+#### Windows
+```shell script
+mvnw spring-boot:run
+```
---
+++ src/test/java/com/wallpapers/server/WallpapersServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallpapers.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallpapersServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
